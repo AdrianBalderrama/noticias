ec6d0963ff31c400be739d6e7a583f34fe05116f
Adrian Balderrama <adrianbalderramab@gmail.com>
2019-11-26 13:19:01 -0800

    segments

--- noticias/src/app/pages/tab2/tab2.page.ts
+++ noticias/src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  
+  @ViewChild(IonSegment) segment: IonSegment;
+  
+  
+categorias = ['general', 'entertainment', 'technology', 'business', 'science', 'sports','health'];
 
-  constructor() {}
 
+ngOnInit(){
+   this.segment.value= this.categorias[0];
+}
 }
