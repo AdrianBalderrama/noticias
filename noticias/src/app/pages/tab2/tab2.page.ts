71d390ed1bd3bab432bb8fb52694b839e102729f
Adrian Balderrama <adrianbalderramab@gmail.com>
2019-11-26 15:12:42 -0800

    cargar categorias display

--- noticias/src/app/pages/tab2/tab2.page.ts
+++ noticias/src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { NoticiasService } from 'src/app/services/noticias.service';
+import { Article } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -13,7 +14,7 @@ export class Tab2Page implements OnInit {
   
   
 categorias = ['entertainment', 'sports','technology', 'business', 'science', 'health'];
-
+noticias: Article[] = []
 
 constructor(private noticiasService: NoticiasService){
 
@@ -21,9 +22,20 @@ constructor(private noticiasService: NoticiasService){
 
 ngOnInit(){
    this.segment.value= this.categorias[0];
-   this.noticiasService.getTopHeadlinesCategoria(this.categorias[0])
-   .subscribe( resp =>{
-      console.log(resp);
-   });
+  this.cargarNoticias(this.categorias[0]);
 }
+  cambioCategoria(event){
+
+      this.noticias = [];
+     this.cargarNoticias(event.detail.value);
+  }
+
+  cargarNoticias(categoria:string){
+     
+    this.noticiasService.getTopHeadlinesCategoria(categoria)
+    .subscribe( resp =>{
+       console.log(resp);
+       this.noticias.push(...resp.articles);
+    });
+  }
 }
