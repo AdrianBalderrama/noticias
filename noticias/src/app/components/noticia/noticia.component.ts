adbb524fb2ce244a1f7aaa4a885fe0b257aab294
Adrian Balderrama <adrianbalderramab@gmail.com>
2019-11-28 14:40:20 -0800

    Action sheet added without functions + dark style,infinite scrolling bug still remains

--- noticias/src/app/components/noticia/noticia.component.ts
+++ noticias/src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+ 
+import { ActionSheetController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-noticia',
@@ -11,17 +14,48 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   
 
-//Open in browser
-  constructor(private iab: InAppBrowser) {
+  constructor(private iab: InAppBrowser,private actionSheetCtrl: ActionSheetController) {
 
    }
 
   ngOnInit() {
 
   }
+  //abrir noticia on browser
   abrirNoticia(){
     const browser = this.iab.create(this.noticia.url, '_system');
    // console.log('Noticia:',this.noticia.url);
   }
 
-}
+
+  async lanzarMenu(){   
+    const actionSheet = await this.actionSheetCtrl.create({
+      
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      },  {
+        text: 'Favoritos',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        cssClass: 'action-dark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+  }
+
